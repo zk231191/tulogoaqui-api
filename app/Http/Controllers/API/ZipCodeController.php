f5b2b8f6ba1ff64a57079b0c2c219753fb15a6d3
Angel Hernandez <angel.hdz91@hotmail.com>
2026-01-27 15:49:17 -0600

    unificar estados QAD

--- app/Http/Controllers/API/ZipCodeController.php
+++ app/Http/Controllers/API/ZipCodeController.php
@@ -8,6 +8,41 @@ use Illuminate\Http\Request;
 
 class ZipCodeController extends Controller
 {
+    private $dipomexEstates = [
+        "AGUASCALIENTES" => "Aguascalientes",
+        "BAJA CALIFORNIA" => "Baja California",
+        "BAJA CALIFORNIA SUR" => "Baja California Sur",
+        "CAMPECHE" => "Campeche",
+        "CHIAPAS" => "Chiapas",
+        "CHIHUAHUA" => "Chihuahua",
+        "CIUDAD DE MEXICO" => "Ciudad de Mexico",
+        "COAHUILA DE ZARAGOZA" => "Coahuila",
+        "COLIMA" => "Colima",
+        "DURANGO" => "Durango",
+        "GUANAJUATO" => "Guanajuato",
+        "GUERRERO" => "Guerrero",
+        "HIDALGO" => "Hidalgo",
+        "JALISCO" => "Jalisco",
+        "MEXICO" => "Estado de Mexico",
+        "MICHOACAN DE OCAMPO" => "Michoacan",
+        "MORELOS" => "Morelos",
+        "NAYARIT" => "Nayarit",
+        "NUEVO LEON" => "Nuevo León",
+        "OAXACA" => "Oaxaca",
+        "PUEBLA" => "Puebla",
+        "QUERETARO" => "Queretaro",
+        "QUINTANA ROO" => "Quintana Roo",
+        "SAN LUIS POTOSI" => "San Luis Potosi",
+        "SINALOA" => "Sinaloa",
+        "SONORA" => "Sonora",
+        "TABASCO" => "Tabasco",
+        "TAMAULIPAS" => "Tamaulipas",
+        "TLAXCALA" => "Tlaxcala",
+        "VERACRUZ DE LA LLAVE" => "Veracruz",
+        "YUCATAN" => "Yucatan",
+        "ZACATECAS" => "Zacatecas",
+    ];
+
     public function show(string $zip)
     {
         if (strlen($zip) !== 5) {
@@ -18,11 +53,19 @@ class ZipCodeController extends Controller
 
         try {
             $data = app(DipomexService::class)->byZip($zip);
+
+            $this->convertEstate($data);
+
             return response()->json($data);
         } catch (\Throwable $e) {
             return response()->json([
                 'message' => $e->getMessage(),
             ], 404);
         }
+    }
+
+    public function convertEstate(&$data): void
+    {
+        $data['estado'] = $this->dipomexEstates[$data['estado']];
     }
 }
